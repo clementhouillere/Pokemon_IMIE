3530ca2fcd322461c96b14525434b0072d23091e
Clement <clement.houillere@gmail.com>
2016-01-14 16:57:39 +0100

    Added blocking stuff in map

--- app/Pokemon_IMIE/Pokemon_IMIE/Pages/Game.xaml.cs
+++ app/Pokemon_IMIE/Pokemon_IMIE/Pages/Game.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using Pokemon_IMIE.usercontrols;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, voir la page http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,35 +12,97 @@ namespace Pokemon_IMIE.Pages
     /// </summary>
     public sealed partial class Game : Page
     {
-        private static int height = 25;
-        private static int width = 30;
+        private int x;
+        private int y;
+        private int[,] tab = new int[,] 
+        {
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+            { 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0},
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            { 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1},
+            { 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1},
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            { 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0},
+            { 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0}
+        };
+        
         public Game()
         {
             this.InitializeComponent();
             Window.Current.Content.KeyDown += move;
+            x = 3;
+            y = 4;
             
         }
         public MainButton RetryButton { get { return this.retry_button; } }
 
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+
         private void move(object sender, KeyRoutedEventArgs e)
         {
-            int size = 50;
-            
+            int height = 32;
+            int width = 32;
             switch (e.Key)
             {
+                
                 case Windows.System.VirtualKey.Space:
                     break;
                 case Windows.System.VirtualKey.D:
-                    this.moveRight();
+                case Windows.System.VirtualKey.Right:
+                    if (tab[x, y + 1] != 1)
+                    {
+                        this.moveRight(width);
+                        y += 1;
+                    }
                     break;
                 case Windows.System.VirtualKey.Q:
-                    this.moveLeft();
+                case Windows.System.VirtualKey.Left:
+                    if (tab[x, y - 1] != 1)
+                    {
+                        this.moveLeft(width);
+                        y -= 1;
+                    }
                     break;
                 case Windows.System.VirtualKey.S:
-                    this.moveDown();
+                case Windows.System.VirtualKey.Down:
+                    if (tab[x + 1, y] != 1)
+                    {
+                        this.moveDown(height);
+                        x += 1;
+                    }
                     break;
                 case Windows.System.VirtualKey.Z:
-                    this.moveUp();
+                case Windows.System.VirtualKey.Up:
+                    if (tab[x - 1, y] != 1)
+                    {
+                        this.moveUp(height);
+                        x -= 1;
+                    }
                     break;
                 
                 default:
@@ -60,19 +111,19 @@ namespace Pokemon_IMIE.Pages
         }
 
 
-        private void moveUp()
+        private void moveUp(int height)
         {
             Canvas.SetTop(map, Canvas.GetTop(map) + height);
         }
-        private void moveDown()
+        private void moveDown(int height)
         {
             Canvas.SetTop(map, Canvas.GetTop(map) - height);
         }
-        private void moveLeft()
+        private void moveLeft(int width)
         {
             Canvas.SetLeft(map, Canvas.GetLeft(map) + width);
         }
-        private void moveRight()
+        private void moveRight(int width)
         {
             Canvas.SetLeft(map, Canvas.GetLeft(map) -width);
         }
@@ -88,6 +139,17 @@ namespace Pokemon_IMIE.Pages
             {
                 success_menu.Visibility = Visibility.Collapsed;
                 success_button.Label = "Succès >";
+            }
+        }
+
+        private void game_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (!e.PreviousSize.Width.Equals(0) && !e.PreviousSize.Height.Equals(0))
+            {
+                int height = int.Parse((e.PreviousSize.Height - e.NewSize.Height).ToString());
+                int width = int.Parse((e.PreviousSize.Width - e.NewSize.Width).ToString());
+                moveRight(width/2);
+                moveDown(height/2);
             }
         }
     }
