ad3f1a73c8cf89dbd158844bd1b78100efffa12d
Clement <clement.houillere@gmail.com>
2016-06-21 15:19:04 +0200

    cleaned app repository, added Base for Model (ClassLibrary), created View and ViewModel folders

---
+++ app/ClassLibrary/Base/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Base
+{
+    public class EntityBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void onPropertyChanged(String name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ app/ClassLibrary/Pokemon.cs
@@ -0,0 +1,73 @@
+﻿using ClassLibrary.Base;
+using System;
+
+namespace ClassLibrary
+{
+    public class Pokemon : EntityBase
+    {
+        private String id;
+        private String name;
+        private String description;
+        private String image;
+
+        public Pokemon(String id, String name, String desc)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Image = this.Id + ".png";
+            this.description = desc;
+        }
+
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public String Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public String Image
+        {
+            get
+            {
+                return image;
+            }
+
+            set
+            {
+                image = value;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+    }
+}
