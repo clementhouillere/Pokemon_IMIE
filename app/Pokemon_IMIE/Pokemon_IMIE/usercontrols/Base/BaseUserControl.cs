536aafea9dc70f17c3bb4301722e65f5a8bdc306
Clement <clement.houillere@gmail.com>
2016-06-22 09:22:20 +0200

    added Base for userControls (not used at the moment)

--- app/Pokemon_IMIE/Pokemon_IMIE/usercontrols/Base/BaseUserControl.cs
+++ app/Pokemon_IMIE/Pokemon_IMIE/usercontrols/Base/BaseUserControl.cs
@@ -4,10 +4,11 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
-namespace ClassLibrary.Base
+namespace TPCSharp.UserControls
 {
-    public class EntityBase : INotifyPropertyChanged
+    public class BaseUserControl : UserControl, INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
